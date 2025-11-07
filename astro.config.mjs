88649055d4204f097aa687cdef76c14d9bbfa048
MohamedJilali <mohamed.el.jilali.abdelkader@student.howest.be>
2025-11-07 12:19:26 +0100

    deploy ready

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://coderpheebs6.github.io',
-    base: '/web3-hackaton',
+  site: 'https://coderpheebs6.github.io',
+  base: '/web3-hackaton',
 });
