1ce7eafe50f8c515798285726d9b37d7216f4eb5
Lambert Kiara <kiara.lambert@student.howest.be>
2025-11-07 11:43:04 +0100

    deploy changes gemaakt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://codepheebs6.github.io/web3-hackaton/',
+    base: '/web3-hackaton/',
+});
