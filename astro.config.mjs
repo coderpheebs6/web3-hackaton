2d43a5c6025055772f8504b95510983f0e8d8c88
Lambert Kiara <kiara.lambert@student.howest.be>
2025-11-07 11:47:20 +0100

    link aangepast

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://codepheebs6.github.io/web3-hackaton/',
+    site: 'https://codepheebs6.github.io',
     base: '/web3-hackaton/',
 });
