8dbb681cb2343ac0a87388f4876fed10d802ce9c
Lambert Kiara <kiara.lambert@student.howest.be>
2025-11-07 11:50:11 +0100

    streepje weg

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://codepheebs6.github.io',
-    base: '/web3-hackaton/',
+    base: '/web3-hackaton',
 });
